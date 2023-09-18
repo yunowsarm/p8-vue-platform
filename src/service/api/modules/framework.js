c75f5be62eba5c59d304b2fb1cb517960c8f6aa0
tangning <1039928300@qq.com>
2023-09-18 15:26:25 +0800

    15437 【前端】5.计划管理--5.1.1.计划列表

--- src/service/api/modules/framework.js
+++ src/service/api/modules/framework.js
@@ -152,6 +152,29 @@ export const system = {
       mockPath: '/framework/system/ThirdPartService/getYear',
       params: {},
       desc: '第三方接口管理>接口异常单条手动处理'
+    },
+    {
+      name: 'distributionAsAWhole',
+      method: 'POST',
+      path: '/product/pm/planManager/distributionAsAWhole',
+      mockPath: '/product/pm/planManager/distributionAsAWhole',
+      params: {},
+      desc: '计划列表>分发统筹'
+    },
+    {
+      name: 'saveLockUnlockForm',
+      method: 'POST',
+      path: '/product/pm/planManager/saveLockUnlockForm',
+      mockPath: '/product/pm/planManager/saveLockUnlockForm',
+      params: {},
+      desc: '计划列表>分发统筹'
+    },
+    {
+      name: 'loadLockUnlockFormData',
+      method: 'POST',
+      path: '/product/pm/planManager/loadLockUnlockFormData',
+      mockPath: '/product/pm/planManager/loadLockUnlockFormData',
+      desc: '项目管理->计划管理->计划加解锁表单数据获取'
     }
   ],
   schedulerTask: [
