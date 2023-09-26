8f821d9f524f32e621d5e92c2436cf9972b9f4a9
wangzhifeng <1047549365@qq.com>
2023-09-26 21:20:48 +0800

    fix: 23861

--- src/views/product/PlanGantt/Components/inputEdit/inputView.vue
+++ src/views/product/PlanGantt/Components/inputEdit/inputView.vue
@@ -1,43 +1,49 @@
 <template>
   <div>
-    <el-tag type="info">前置输出物</el-tag>
-    <div v-if="data">
-      <div v-for="(item, index) in data" :key="index">
-        <el-row type="flex">
-          <el-col :span="3" class="baseTitle">任务名称</el-col>
-          <el-col :span="21" class="baseContent">{{ item.taskName }}</el-col
-          ><br />
-        </el-row>
-        <div v-for="(input, inIndex) in item.inputDatas" :key="inIndex + index">
-          <el-row type="flex">
-            <el-col :span="3" class="baseTitle">输入要求</el-col>
-            <el-col :span="21" class="baseContent">{{ input.describes }}</el-col
-            ><br />
-          </el-row>
-          <el-row type="flex">
-            <el-col :span="3" class="baseTitle">补充要求</el-col>
-            <el-col :span="21" class="baseContent">
-              <div v-for="attr in input.uploadFiles" :key="attr.label" :value="attr.value">
-                <el-row type="flex">
-                  <el-col :span="12" class="baseTitle"
-                    ><span @click="downloadOutputRequsetFile(attr)">{{ attr.fileName }}</span></el-col
-                  >
-                  <el-col :span="12" class="baseContent">{{ attr.confidentialite }}</el-col
-                  ><br />
-                </el-row>
-              </div>
-            </el-col>
-          </el-row>
+    <el-tabs v-model="activeOutput" type="border-card">
+      <el-tab-pane label="增加的输入物" name="inputKey">
+        <span slot="label"><i class="p8 icon-shuchuyaoqiu"></i> 增加的输入物</span>
+        <div style="position: relative; padding-bottom: 50px">
+          <form-list ref="form" @rendered="rendered" form-layout="vertical" :data-source="dataSource" :api="saveApi" :form="formData" :exist-default-btn="existDefaultBtn" :other-param="otherParam">
+          </form-list>
         </div>
-        <el-divider></el-divider>
-      </div>
-    </div>
-    <div v-else style="height: 24px"></div>
-    <el-tag>增加的输入物</el-tag>
-    <div style="position: relative; padding-bottom: 50px">
-      <form-list ref="form" @rendered="rendered" form-layout="vertical" :data-source="dataSource" :api="saveApi" :form="formData" :exist-default-btn="existDefaultBtn" :other-param="otherParam">
-      </form-list>
-    </div>
+      </el-tab-pane>
+      <el-tab-pane label="前置输出物" name="getPreOutputKey">
+        <span slot="label"><i class="p8 icon-xuanxiang1"></i> 前置输出物</span>
+        <div v-if="data">
+          <div v-for="(item, index) in data" :key="index">
+            <el-row type="flex">
+              <el-col :span="3" class="baseTitle">任务名称</el-col>
+              <el-col :span="21" class="baseContent">{{ item.taskName }}</el-col
+              ><br />
+            </el-row>
+            <div v-for="(input, inIndex) in item.inputDatas" :key="inIndex + index">
+              <el-row type="flex">
+                <el-col :span="3" class="baseTitle">输入要求</el-col>
+                <el-col :span="21" class="baseContent">{{ input.describes }}</el-col
+                ><br />
+              </el-row>
+              <el-row type="flex">
+                <el-col :span="3" class="baseTitle">补充要求</el-col>
+                <el-col :span="21" class="baseContent">
+                  <div v-for="attr in input.uploadFiles" :key="attr.label" :value="attr.value">
+                    <el-row type="flex">
+                      <el-col :span="12" class="baseTitle"
+                        ><span @click="downloadOutputRequsetFile(attr)">{{ attr.fileName }}</span></el-col
+                      >
+                      <el-col :span="12" class="baseContent">{{ attr.confidentialite }}</el-col
+                      ><br />
+                    </el-row>
+                  </div>
+                </el-col>
+              </el-row>
+            </div>
+            <el-divider></el-divider>
+          </div>
+        </div>
+        <div v-else style="height: 24px"></div>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 <style scoped>
@@ -79,6 +85,7 @@ export default {
   data() {
     return {
       data: [],
+      activeOutput: 'inputKey',
       saveApi: 'planGanttManager.inputSave',
       isCustomValidate: true,
       existDefaultBtn: false,
