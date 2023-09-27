a6954f316adfce193d0b9eb9e48071053ea99ef2
mengyi <13572165143@163.com>
2023-09-27 17:56:02 +0800

    feat:15577 【前端】5.1.2.计划执行--5.1.2.计划执行

---
+++ src/views/product/My/Work/Task/Components/planCarriedout.vue
@@ -0,0 +1,126 @@
+<template>
+  <left-center-right-layout :percentRight="65"
+                            v-if="isRouterShow">
+    <template #left>
+      <div class="task-info-con">
+        <!-- 任务详情信息 -->
+        <task-info-view></task-info-view>
+      </div>
+    </template>
+    <template #center>
+      <div class="task-operating-con">
+        <div class="task-manage-table">
+          <!-- 管理要素 -->
+          <task-manage-view :thirdMenuParam="thirdMenuParam"></task-manage-view>
+        </div>
+        <div class="task-tabs-con">
+          <!-- 进度反馈-未完成原因 -->
+          <task-tabs-view></task-tabs-view>
+        </div>
+      </div>
+    </template>
+    <template #right>
+      <div class="task-info-related-con">
+        <!-- 关联任务 -->
+        <task-relation-view></task-relation-view>
+      </div>
+    </template>
+  </left-center-right-layout>
+</template>
+<script>
+import LeftCenterRightLayout from './layout/LeftCenterRight'
+import TaskTabsView from './taskOperating/TaskTabs'
+import TaskInfoView from './taskInfo'
+import TaskManageView from './taskManage/index'
+import TaskRelationView from './taskRelation/index'
+export default {
+  name: 'planExecute',
+  provide () { // 使用 provide对深层组件进行数据信息传递 例:taskOperating/Progress.vue中 inject搭配computed接收数据
+    return {
+      getPlanInfo: () => this.thirdMenuParamTemp
+    }
+  },
+  props: {
+    thirdMenuParam: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    }
+  },
+  watch: {
+    thirdMenuParam: {
+      handler (val) {
+        this.thirdMenuParamTemp = val
+      },
+      immediate: true,
+      deep: true
+    },
+    $route (val, oldVal) {
+      this.reload()
+    }
+  },
+  data () {
+    return {
+      isRouterShow: true,
+      secretLevel: '机密',
+      thirdMenuParamTemp: this.thirdMenuParam
+    }
+  },
+  created () {
+  },
+  mounted () {
+    // this.reload()
+  },
+  methods: {
+    async reload () {
+      this.isRouterShow = false
+      await this.getTaskInfo()
+    },
+    async getTaskInfo () {
+      let _this = this
+
+    }
+  },
+  components: {
+    LeftCenterRightLayout,
+    TaskTabsView,
+    TaskInfoView,
+    TaskManageView,
+    TaskRelationView
+  }
+}
+</script>
+<style lang="scss" scoped>
+.secret_level {
+  position: absolute;
+  right: 20px;
+  top: 12px;
+  z-index: 9;
+  font-weight: bolder;
+  // top: 50%;
+  // transform: translateY(-50%);
+  color: red;
+  font-size: larger;
+}
+div.task-info-con {
+  height: 100%;
+  background-color: #ffffff;
+}
+div.task-operating-con {
+  height: 100%;
+  div.task-manage-table {
+    width: 100%;
+    height: 30%;
+    background-color: #ffffff;
+  }
+  div.task-tabs-con {
+    height: 70%;
+    background-color: #ffffff;
+  }
+}
+div.task-info-related-con {
+  height: 100%;
+  background-color: #ffffff;
+}
+</style>
---
+++ src/views/product/My/Work/Task/Components/taskManage/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <pane-view paneTitle="管理要素"
+             icon="p8 icon-guanliyaosu">
+    <template #paneTitle>
+      <div></div>
+    </template>
+    <template #paneInfo>
+      <div class="task-manage-con">
+        <list-view :thirdMenuParam="thirdMenuParam"></list-view>
+      </div>
+    </template>
+  </pane-view>
+</template>
+<script>
+import PaneView from '../layout/Pane'
+import ListView from './list'
+export default {
+  name: 'TaksManager',
+  inject: ['getPlanInfo'],
+  components: {
+    PaneView,
+    ListView
+  },
+  props: {
+    thirdMenuParam: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {}
+  },
+  mounted () {
+
+  },
+  methods: {}
+}
+</script>
+
