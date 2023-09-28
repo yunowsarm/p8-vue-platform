b6a4ca102eed88c4245efc2e3b22acfdf1d8a5a1
wangzhifeng <1047549365@qq.com>
2023-09-28 18:02:23 +0800

    feat: 15536 【前端】4.4.3.计划编制-统计信息

--- src/components/gantt/Components/CommandStatistic/index.vue
+++ src/components/gantt/Components/CommandStatistic/index.vue
@@ -1,16 +1,50 @@
 <template>
-  <div class="box">
-    <div id="ganttChart"></div>
-    <el-divider class="line" direction="vertical"></el-divider>
-    <div class="detail_box">
-      <div>
-        <div class="total_num">{{ totalNum }}</div>
-        <div class="tital_text">任务标识总数</div>
+  <div>
+    <div class="box">
+      <div id="monitorChart"></div>
+      <el-divider class="line" direction="vertical"></el-divider>
+      <div class="detail_box">
+        <div>
+          <div class="total_num">{{ totalNum }}</div>
+          <div class="tital_text">任务标识总数</div>
+        </div>
+        <div class="monitor_list">
+          <div class="monitor_item" v-for="(item, index) in list" :key="index">
+            <span :class="item.icon"></span>
+            <span class="text">{{ item.value }}</span>
+          </div>
+        </div>
       </div>
-      <div class="monitor_list">
-        <div class="monitor_item" v-for="(item, index) in list" :key="index">
-          <span :class="item.icon"></span>
-          <span class="text">{{ item.value }}</span>
+    </div>
+    <div class="box">
+      <div id="statusChart"></div>
+      <el-divider class="line" direction="vertical"></el-divider>
+      <div class="detail_box">
+        <div>
+          <div class="total_num">{{ totalStatusNum }}</div>
+          <div class="tital_text">任务状态总数</div>
+        </div>
+        <div class="monitor_list">
+          <div class="monitor_item" v-for="(item, index) in list2" :key="index">
+            <span :class="item.icon"></span>
+            <span class="text">{{ item.value }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <div id="typeChart"></div>
+      <el-divider class="line" direction="vertical"></el-divider>
+      <div class="detail_box">
+        <div>
+          <div class="total_num">{{ totalTypeNum }}</div>
+          <div class="tital_text">任务状态总数</div>
+        </div>
+        <div class="monitor_list">
+          <div class="monitor_item" v-for="(item, index) in list3" :key="index">
+            <span :class="item.icon"></span>
+            <span class="text">{{ item.value }}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -24,8 +58,17 @@ export default {
   data() {
     return {
       list: [],
+      list2: [],
+      list3: [],
       myChart: null,
-      initOptionData: {}
+      myChart2: null,
+      myChart3: null,
+      initOptionData: {},
+      initOptionData2: {},
+      initOptionData3: {},
+      totalNum: 0,
+      totalStatusNum: 0,
+      totalTypeNum: 0
     }
   },
   props: {
@@ -39,10 +82,12 @@ export default {
     }
   },
   created() {
-    this.getData()
+    this.getDataMonitor()
+    this.getDataStatus()
+    this.getDataTaskType()
   },
   methods: {
-    getData() {
+    getDataMonitor() {
       const that = this
       this.totalNum = 0
       this.$api['planGanttManager.getPlanStatisticData']({ planInfoId: this.planInfoId }).then((res) => {
@@ -98,14 +143,146 @@ export default {
           ]
         }
         that.initOptionData = option
-        that.initChart()
+        that.initMonitorChart()
       })
     },
-    initChart() {
+    initMonitorChart() {
       // 基于准备好的dom，初始化echarts实例
-      this.myChart = echarts.init(document.getElementById('ganttChart'))
+      this.myChart = echarts.init(document.getElementById('monitorChart'))
       // 绘制图表
       this.myChart.setOption(this.initOptionData)
+    },
+    getDataStatus() {
+      const that = this
+      this.$api['planGanttManager.getPlanStatusData']({ planInfoId: this.planInfoId }).then((res) => {
+        if (res) {
+          this.totalStatusNum = 0
+          this.list2 = res.map((ele) => {
+            this.totalNum = ele.totalCount || 0
+            this.totalStatusNum += Number(ele.taskCount) || 0
+            return {
+              icon: ele.icon,
+              name: ele.name,
+              value: ele.taskCount
+            }
+          })
+        }
+        const option = {
+          tooltip: {
+            trigger: 'item'
+          },
+          legend: {
+            type: 'scroll',
+            orient: 'vertical',
+            right: 20,
+            top: 20,
+            bottom: 20,
+            icon: 'circle',
+            itemWidth: 10,
+            itemHeight: 10,
+            formatter: function (name) {
+              return name
+            }
+          },
+          series: [
+            {
+              type: 'pie',
+              radius: ['40%', '70%'],
+              center: ['30%', '50%'],
+              avoidLabelOverlap: false,
+              itemStyle: {
+                borderRadius: 10,
+                borderColor: '#fff',
+                borderWidth: 2
+              },
+              label: {
+                show: true,
+                position: 'center',
+                formatter: `${this.totalNum}\n任务总数`,
+                fontSize: '13'
+              },
+              labelLine: {
+                show: false
+              },
+              data: this.list2
+            }
+          ]
+        }
+        that.initOptionData2 = option
+        that.initStatusChart()
+      })
+    },
+    initStatusChart() {
+      // 基于准备好的dom，初始化echarts实例
+      this.myChart2 = echarts.init(document.getElementById('statusChart'))
+      // 绘制图表
+      this.myChart2.setOption(this.initOptionData2)
+    },
+    getDataTaskType() {
+      const that = this
+      this.$api['planGanttManager.getPlanTaskTypeData']({ planInfoId: this.planInfoId }).then((res) => {
+        if (res) {
+          this.totalTypeNum = 0
+          this.list3 = res.map((ele) => {
+            this.totalNum = ele.totalCount || 0
+            this.totalTypeNum += Number(ele.taskCount) || 0
+            return {
+              icon: ele.icon,
+              name: ele.name,
+              value: ele.taskCount
+            }
+          })
+        }
+        const option = {
+          tooltip: {
+            trigger: 'item'
+          },
+          legend: {
+            type: 'scroll',
+            orient: 'vertical',
+            right: 20,
+            top: 20,
+            bottom: 20,
+            icon: 'circle',
+            itemWidth: 10,
+            itemHeight: 10,
+            formatter: function (name) {
+              return name
+            }
+          },
+          series: [
+            {
+              type: 'pie',
+              radius: ['40%', '70%'],
+              center: ['30%', '50%'],
+              avoidLabelOverlap: false,
+              itemStyle: {
+                borderRadius: 10,
+                borderColor: '#fff',
+                borderWidth: 2
+              },
+              label: {
+                show: true,
+                position: 'center',
+                formatter: `${this.totalNum}\n任务总数`,
+                fontSize: '13'
+              },
+              labelLine: {
+                show: false
+              },
+              data: this.list3
+            }
+          ]
+        }
+        that.initOptionData3 = option
+        that.initTaskTypeChart()
+      })
+    },
+    initTaskTypeChart() {
+      // 基于准备好的dom，初始化echarts实例
+      this.myChart3 = echarts.init(document.getElementById('typeChart'))
+      // 绘制图表
+      this.myChart3.setOption(this.initOptionData3)
     }
   }
 }
@@ -116,9 +293,17 @@ export default {
   display: flex;
   align-items: center;
   height: 140px;
-  #ganttChart {
+  #monitorChart {
     height: 100%;
-    width: 240px;
+    width: 300px;
+  }
+  #statusChart {
+    height: 100%;
+    width: 300px;
+  }
+  #typeChart {
+    height: 100%;
+    width: 300px;
   }
   .line {
     margin: 0 16px;
