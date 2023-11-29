1bfc44e154fb19aed21b5443c86534c2ad0e7721
mengyi <13572165143@163.com>
2023-11-29 18:28:54 +0800

    fix:审批页面 表格传参优化

--- src/views/Framework/ComponentsMananger/Grid/Components/tableRender.vue
+++ src/views/Framework/ComponentsMananger/Grid/Components/tableRender.vue
@@ -1,6 +1,6 @@
 <template>
   <normal-layout class="grid-table-render"
-                 :header-visible="headerVisible"
+                 :header-visible="headerVisibleType"
                  :normal-layout="normalLayout">
     <template #north>
       <common-button v-if="tableInfo.useSystemConfigButton == 1 || (tableInfo.useSystemConfigButton == 0 && buttonData.length > 0)"
@@ -428,6 +428,10 @@ export default {
     taskId: {
       type: String,
       default: ''
+    },
+    pageType: {
+      type: String,
+      default: ''
     }
   },
   inject: {
@@ -551,7 +555,8 @@ export default {
       customCSS: {},
       sysParams: Object.assign({ $SYSTEM_PARAMS_SELECT: _cloneDeep(this.$store.state.user.userInfo) }), // 系统级参数
       currentRouterPath: '',
-      serachForm: {}
+      serachForm: {},
+      headerVisibleType: this.headerVisible
     }
   },
   created () {
@@ -567,6 +572,10 @@ export default {
     )
   },
   mounted () {
+    // 我的审批页面表格不展示按钮
+    if (this.pageType === 'view') {
+      this.headerVisibleType = false
+    }
     if (this.code && this.code !== '') {
       this.getTableInfo(this.code)
     }
