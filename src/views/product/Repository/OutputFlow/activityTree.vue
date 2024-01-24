98d8fea3ff7edaffe0273cbaf9ff9d84903917b5
tangning <1039928300@qq.com>
2024-01-24 19:31:47 +0800

    fix: 产出流程管理-计划加载重复问题修改

--- src/views/product/Repository/OutputFlow/activityTree.vue
+++ src/views/product/Repository/OutputFlow/activityTree.vue
@@ -513,6 +513,7 @@ export default {
     }
   },
   beforeDestroy () {
+    GanttObject.setGanttObject('activityGantt', {})
     myGantt.unselectTask()
     myGantt.parse({ tasks: [] })
     this.selectedTasks = []
