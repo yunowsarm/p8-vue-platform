0f31de4cffa150efed7fa232186a53af92afc87b
tangning <1039928300@qq.com>
2024-03-21 10:03:37 +0800

    feat: 18399 看板管理-调度会看板-综合看板--看板管理调度会看板，首次点击菜单右侧展示页面左侧对应树节点显示选中，以及表格默认参数传参方式修改

--- src/views/Framework/ComponentsMananger/Layout/Components/ButtonNavigation/V1.0/view/index.vue
+++ src/views/Framework/ComponentsMananger/Layout/Components/ButtonNavigation/V1.0/view/index.vue
@@ -183,9 +183,36 @@ export default {
           }
         })
       } else {
+        const that = this
         this.asyncComponents = defaultComponents.url ? defaultComponents.url : ''
         this.componentsConfig = defaultComponents
+        this.$nextTick(() => {
+          let nodeId = that.setDefaultNode(defaultComponents)
+          that.$refs.commonTree.$refs.tree.setCurrentKey(nodeId, true)
+        })
       }
+    },
+    setDefaultNode (defaultComponents) {
+      let nodeId = ''
+      function getDefalutNode (list) {
+        list.forEach(el => {
+          let code = ''
+          if (el.componentsUrl) {
+            let parmars = el.componentsUrl.split('?')[1]
+            if (parmars) {
+              code = parmars.split('=')[1]
+            }
+          }
+          if (code == defaultComponents.code || el.componentsName == defaultComponents.name) {
+            nodeId = el.id
+          }
+          if (el.children && el.children.length) {
+            getDefalutNode(el.children)
+          }
+        })
+      }
+      getDefalutNode(this.treeData)
+      return nodeId
     },
     getFirstChild (data) {
       let result = ''
