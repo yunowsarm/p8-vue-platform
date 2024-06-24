9245bcd7aa82fd8e2e724ae57c70448cd965d649
tangning <1039928300@qq.com>
2024-06-24 14:47:31 +0800

    28241 我的待办提交审批后，待办事项过长导致我的审批页面显示异常

--- src/views/Framework/System/TodoList/approverForm.vue
+++ src/views/Framework/System/TodoList/approverForm.vue
@@ -1,16 +1,16 @@
 <template>
   <form-list ref="form"
-             :dataSource="dataSource"
+             class="form"
+             :data-source="dataSource"
              @rendered="rendered"
-             labelWidth="90px"
-             :existDefaultBtn="false"
-             :form="formData">
-  </form-list>
+             label-width="90px"
+             :exist-default-btn="false"
+             :form="formData"> </form-list>
 </template>
 <script>
 import { P8Form as FormList } from 'p8-components-ui'
 export default {
-  name: 'approverForm',
+  name: 'ApproverForm',
   props: {
     businessKey: {
       type: String,
@@ -36,7 +36,7 @@ export default {
         {
           type: 'view',
           labelText: '待办事项内容',
-          colLayout: 'doubleCol',
+          colLayout: 'singleCol',
           fieldName: 'content'
         },
         {
@@ -91,15 +91,15 @@ export default {
           type: 'view',
           labelText: '完成情况',
           fieldName: 'execution',
-          colLayout: 'doubleCol'
-        },
+          colLayout: 'singleCol'
+        }
       ]
     }
   },
   methods: {
     rendered () {
       if (this.businessKey) {
-        this.$api['TodoList.list']({ id: this.businessKey }).then(res => {
+        this.$api['TodoList.list']({ id: this.businessKey }).then((res) => {
           this.formData = res
         })
       }
@@ -111,4 +111,8 @@ export default {
 }
 </script>
 <style lang="scss" scpoed>
+// ::v-deep
+.form .el-form-item__content .view {
+  word-break: break-word;
+}
 </style>
