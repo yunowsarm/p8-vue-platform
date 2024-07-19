879bde3481dc94311974a42c8163a6043234dd55
wangzhifeng <1047549365@qq.com>
2024-07-19 09:31:37 +0800

    fix: 计划编制-任务查看时显示绩效和比例

--- src/views/product/PlanGantt/Components/describeEdit/describeView.vue
+++ src/views/product/PlanGantt/Components/describeEdit/describeView.vue
@@ -127,6 +127,18 @@ export default {
           options: []
         },
         {
+          type: 'view',
+          labelText: '绩效',
+          fieldName: 'achievements',
+          colLayout: 'doubleCol'
+        },
+        {
+          type: 'view',
+          labelText: '比例',
+          fieldName: 'proportion',
+          colLayout: 'doubleCol'
+        },
+        {
           labelText: '预计开始时间',
           type: 'view',
           fieldName: 'forecastBeginDate',
@@ -216,6 +228,13 @@ export default {
       }
       that.formData.autoScheduling = task.autoScheduling
       that.formData.duration = task.duration
+      that.formData.achievements = task.achievements
+      let parts = task.proportion.toString().split('.')
+      var fraction = parts.length === 1 ? '' : parts[1];
+      if (2 > fraction.length) {
+          fraction += new Array(2 - fraction.length + 1).join('0');
+      }
+      that.formData.proportion = parts[0] + '.' + fraction + '%'
       that.formData.planType = task.planType
       that.formData.forecastBeginDate = moment(task.forecastBeginDate).format('YYYY-MM-DD')
       that.formData.forecastEndDate = moment(task.forecastEndDate).format('YYYY-MM-DD')
