fd9f441ec9963916e367a00c3ba133d5a6940aba
tangning <1039928300@qq.com>
2024-11-21 14:18:54 +0800

    29370 外部链接图标的颜色设置不生效

--- src/views/Framework/System/externalLinks/edit.vue
+++ src/views/Framework/System/externalLinks/edit.vue
@@ -30,6 +30,7 @@
           <template #dialog>
             <icon-selector @icon-select="iconSelect"
                            :selected-name="modify.icon"
+                           :selectedColor="modify.color"
                            :color-picker="true"></icon-selector>
           </template>
         </common-dialog>
