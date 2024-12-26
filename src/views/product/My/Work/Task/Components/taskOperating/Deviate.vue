e947bfaac301709de16e114603b1fe0204cf62e3
mengyi <13572165143@163.com>
2024-12-26 15:56:25 +0000

    Revert "feat:1.6.1.5升级平台版本（平台版本011）基于10645所有业务实体类继承PO基类并实现相关功能（含低代码平台）"
    This reverts commit 055911997724b9b03102d20815b00a54733b7989

--- src/views/product/My/Work/Task/Components/taskOperating/Deviate.vue
+++ src/views/product/My/Work/Task/Components/taskOperating/Deviate.vue
@@ -75,7 +75,7 @@ export default {
       },
       {
         title: '反馈时间',
-        dataIndex: 'itemCreateTime',
+        dataIndex: 'createTime',
         align: 'left'
       }
     ]
--- src/views/product/My/Work/Task/Components/taskOperating/UnfinishedCause.vue
+++ src/views/product/My/Work/Task/Components/taskOperating/UnfinishedCause.vue
@@ -68,7 +68,7 @@ export default {
       },
       {
         title: '反馈时间',
-        dataIndex: 'itemCreateTime',
+        dataIndex: 'createTime',
         align: 'left'
       }
     ]
