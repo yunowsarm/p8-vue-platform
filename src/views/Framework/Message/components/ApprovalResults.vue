dab43780d8c41f9d9bd48eaaa2a0594a1649a47f
liangmengyao <lmy_cn@163.com>
2024-12-27 10:29:31 +0800

    fix：我的消息-审批结束后发送消息，消息视图显示审批详情功能逻辑优化

---
+++ src/views/Framework/Message/components/ApprovalResults.vue
@@ -0,0 +1,71 @@
+<script>
+import History from '@/views/Framework/BusinessActivity/ProcessApproval/history.vue'
+
+export default {
+  name: 'ApprovalResults',
+  components: { History },
+  props: {
+    taskId: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      approvalData: null,
+      isDataLoaded: false
+    }
+  },
+  created () {
+    this.getApprovalData(this.taskId)
+  },
+  methods: {
+    async getApprovalData (taskId) {
+      try {
+        if (!taskId) {
+          console.error('Task ID is invalid')
+          return
+        }
+
+        const [data] = await this.$api['PersonalProcessApproval.getApproveContentViewUrl']({ taskId: taskId })
+        debugger
+        if (!data || !data.processInstanceId) {
+          console.warn('Invalid or missing data from API')
+          return
+        }
+
+        const { records } = await this.$api['PersonalProcessApproval.processHistoryList']({
+          processInstId: data.processInstanceId,
+          businessKey: data.value.businessKey || data.value.changeId,
+          page: {
+            current: 1,
+            size: 5,
+            orders: []
+          }
+        })
+        debugger
+
+        if (records && records.length > 0) {
+          this.approvalData = records[0]
+        } else {
+          console.warn('No records found in the response')
+        }
+
+        this.isDataLoaded = true // 数据加载完成后设置标志位为 true
+      } catch (error) {
+        console.error('Error fetching approval data:', error)
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <History v-if="isDataLoaded" :selected-approval="approvalData" /> <!-- 根据标志位条件渲染 History 组件 -->
+  </div>
+</template>
+
+<style scoped lang="scss">
+/* 你的样式代码 */
+</style>
