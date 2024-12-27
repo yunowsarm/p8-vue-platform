31440988e46951f3cd6663c022774fcc35ec1250
liangmengyao <lmy_cn@163.com>
2024-12-27 20:50:42 +0800

    fix（计划管理）：计划编辑锁定开关临时隐藏

--- src/views/product/Plan/lockUnlockForm.vue
+++ src/views/product/Plan/lockUnlockForm.vue
@@ -1,65 +1,34 @@
 <template>
-  <el-form ref="form"
-           :model="formData"
-           label-width="100px"
-           class="formList"
-           :inline-Message="inlineMessage"
-           style="height:100%">
-    <el-row :class="{'existBtn':true}">
+  <el-form ref="form" :model="formData" label-width="100px" class="formList" :inline-message="inlineMessage" style="height: 100%">
+    <el-row :class="{ existBtn: true }">
       <template v-for="(item, index) in dataSource">
-        <el-col :span="item.colLayout === 'doubleCol'?12:24"
-                :key="index">
-          <el-form-item v-if="item.labelText === '项目状态'"
-                        :label="item.labelText"
-                        :prop="item.fieldName">
-            <el-radio-group :disabled="taskEditCheck"
-                            v-model="formData[item.fieldName]">
-              <el-radio-button v-for="opt in zzOptions"
-                               :key="opt.value"
-                               :label="opt.value"
-                               :value="opt.value">{{opt.label}}</el-radio-button>
+        <el-col :span="item.colLayout === 'doubleCol' ? 12 : 24" :key="index">
+          <el-form-item v-if="item.labelText === '项目状态'" :label="item.labelText" :prop="item.fieldName">
+            <el-radio-group :disabled="taskEditCheck" v-model="formData[item.fieldName]">
+              <el-radio-button v-for="opt in zzOptions" :key="opt.value" :label="opt.value" :value="opt.value">{{ opt.label }}</el-radio-button>
             </el-radio-group>
           </el-form-item>
-          <el-form-item v-else
-                        :label="item.labelText"
-                        :prop="item.fieldName">
-            <el-radio-group :disabled="taskEditCheck"
-                            v-if="item.fieldName!=='1008'"
-                            v-model="formData[item.fieldName]">
-              <el-radio-button v-for="opt in options"
-                               :key="opt.value"
-                               :label="opt.value"
-                               :value="opt.value">{{opt.label}}</el-radio-button>
+          <el-form-item v-else :label="item.labelText" :prop="item.fieldName">
+            <el-radio-group :disabled="taskEditCheck" v-if="item.fieldName !== '1008'" v-model="formData[item.fieldName]">
+              <el-radio-button v-for="opt in options" :key="opt.value" :label="opt.value" :value="opt.value">{{ opt.label }}</el-radio-button>
             </el-radio-group>
-            <el-radio-group :disabled="taskEditCheck"
-                            v-else
-                            v-model="formData[item.fieldName]">
-              <el-radio-button v-for="opt in options2"
-                               :key="opt.value"
-                               :label="opt.value"
-                               :value="opt.value">{{opt.label}}</el-radio-button>
+            <el-radio-group :disabled="taskEditCheck" v-else v-model="formData[item.fieldName]">
+              <el-radio-button v-for="opt in options2" :key="opt.value" :label="opt.value" :value="opt.value">{{ opt.label }}</el-radio-button>
             </el-radio-group>
           </el-form-item>
         </el-col>
       </template>
-           <el-col>
-             <el-form-item label="解锁/锁定原因" prop="content" :rules="validator(rules)">
-               <el-input
-                   type="textarea"
-                   placeholder="请输入解锁/锁定原因"
-                   v-model="formData['content']"
-                   rows="4"
-               ></el-input>
-             </el-form-item>
-           </el-col>
+      <el-col>
+        <el-form-item label="解锁/锁定原因" prop="content" :rules="validator(rules)">
+          <el-input type="textarea" placeholder="请输入解锁/锁定原因" v-model="formData['content']" rows="4"></el-input>
+        </el-form-item>
+      </el-col>
     </el-row>
-    <el-row :class="{'formBtn':true}">
+    <el-row :class="{ formBtn: true }">
       <el-col class="flex-right">
         <slot name="btn"></slot>
-        <el-button plain
-                   @click="$emit('close-lock-unlock', {})">取消</el-button>
-        <el-button type="primary"
-                   @click="handleSubmit">确定</el-button>
+        <el-button plain @click="$emit('close-lock-unlock', {})">取消</el-button>
+        <el-button type="primary" @click="handleSubmit">确定</el-button>
       </el-col>
     </el-row>
   </el-form>
@@ -68,7 +37,7 @@
 <script>
 import { Form, FormItem, Row, Col, Button, RadioGroup, RadioButton } from 'p8-components-ui'
 export default {
-  name: 'lockUnlockForm',
+  name: 'LockUnlockForm',
   props: ['planInfoArray', 'content', 'showMonitorPoints', 'monitorDutyType', 'monitorApi'],
   components: {
     'el-form': Form,
@@ -78,9 +47,8 @@ export default {
     'el-button': Button,
     'el-radio-group': RadioGroup,
     'el-radio-button': RadioButton
-
   },
-  data () {
+  data() {
     return {
       saveApi: 'thirdPartInterface.saveLockUnlockForm',
       otherParam: {},
@@ -91,7 +59,7 @@ export default {
       isCustomValidate: true,
       inlineMessage: true,
       formData: {
-        content:'',
+        content: ''
       },
       monitorPoints: {},
       monitorPointLockUnlockStatus: {},
@@ -155,7 +123,7 @@ export default {
     }
   },
   mounted: function () {
-    let that = this
+    const that = this
     if (that.monitorDutyType) {
       this.options = [
         {
@@ -188,106 +156,115 @@ export default {
       ]
     }
     that.otherParam.planInfoArray = that.planInfoArray
-    that.$api[that.monitorApi]({ planInfoArray: that.planInfoArray }).then(res => {
-      if (res && res.length) {
-        that.monitorPoints = res
-        res.forEach(function (item, idx) {
-          if (item.value !== 'taskEdit') {
-            let dataSourceItem = {
-              type: 'radioButton',
-              labelText: item.label,
-              fieldName: item.value,
-              colLayout: 'doubleCol',
+    that.$api[that.monitorApi]({ planInfoArray: that.planInfoArray })
+      .then((res) => {
+        if (res && res.length) {
+          that.monitorPoints = res
+          res.forEach(function (item, idx) {
+            // TODO:1020为计划编辑  临时隐藏
+            if (item.value !== 'taskEdit' && item.value !== '1020') {
+              const dataSourceItem = {
+                type: 'radioButton',
+                labelText: item.label,
+                fieldName: item.value,
+                colLayout: 'doubleCol'
+              }
+              that.dataSource.push(dataSourceItem)
+              that.$set(that.formData, item.value, item.lockStatus)
             }
-            that.dataSource.push(dataSourceItem)
-            that.$set(that.formData, item.value, item.lockStatus)
-          }
-        })
-        that.formData.content = that.monitorPoints[0].content
-        that.showMonitorPoints && that.showMonitorPoints.length && (that.dataSource = that.dataSource.filter((item) => {
-          return that.showMonitorPoints.includes(item.fieldName)
-        }))
-      }
-    }).catch(function (error) {
-      console.error(error)
-    })
-
+          })
+          that.formData.content = that.monitorPoints[0].content
+          that.showMonitorPoints &&
+            that.showMonitorPoints.length &&
+            (that.dataSource = that.dataSource.filter((item) => {
+              return that.showMonitorPoints.includes(item.fieldName)
+            }))
+        }
+      })
+      .catch(function (error) {
+        console.error(error)
+      })
   },
   methods: {
-    taskEditChange (obj) {
+    taskEditChange(obj) {
       if (obj === '1') {
         this.taskEditCheck = true
       } else {
         this.taskEditCheck = false
       }
     },
-    handleSubmit (e) {
+    handleSubmit(e) {
       e.preventDefault()
-      let that = this
-      that.validate().then((queryParams) => {
-        let saveParams = { ...queryParams, ...that.otherParam }
-        let formData = saveParams
-        that.monitorPoints.forEach(function (item, idx) {
-          if (saveParams[item.value]) {
-            if (item.value === '1025') {
-              that.terminationSuspendStatus[item.value] = saveParams[item.value]
+      const that = this
+      that
+        .validate()
+        .then((queryParams) => {
+          const saveParams = { ...queryParams, ...that.otherParam }
+          const formData = saveParams
+          that.monitorPoints.forEach(function (item, idx) {
+            if (saveParams[item.value]) {
+              if (item.value === '1025') {
+                that.terminationSuspendStatus[item.value] = saveParams[item.value]
+              } else {
+                that.monitorPointLockUnlockStatus[item.value] = saveParams[item.value]
+              }
+            }
+          })
+          // 任务编辑
+          that.monitorPoints.forEach(function (item, idx) {
+            // 若任务编辑为加锁状态时，标识设置为加锁状态（表单禁用）
+            if (saveParams.taskEdit === '1') {
+              that.monitorPointLockUnlockStatus[item.value] = '1'
             } else {
-              that.monitorPointLockUnlockStatus[item.value] = saveParams[item.value]
+              if (saveParams[item.value] && item.value !== '1025') {
+                that.monitorPointLockUnlockStatus[item.value] = saveParams[item.value]
+              }
             }
+          })
+          that.monitorPointLockUnlockStatus.taskEdit = saveParams.taskEdit
+          if (JSON.stringify(that.monitorPointLockUnlockStatus) === '{}' && JSON.stringify(that.terminationSuspendStatus) === '{}') {
+            that.$message({ message: `请选择内容`, type: 'error' })
           }
-        })
-        // 任务编辑
-        that.monitorPoints.forEach(function (item, idx) {
-          // 若任务编辑为加锁状态时，标识设置为加锁状态（表单禁用）
-          if (saveParams['taskEdit'] === '1') {
-            that.monitorPointLockUnlockStatus[item.value] = '1'
-          } else {
-            if (saveParams[item.value] && item.value !== '1025') {
-              that.monitorPointLockUnlockStatus[item.value] = saveParams[item.value]
-            }
+          if (JSON.stringify(that.monitorPointLockUnlockStatus) !== '{}') {
+            formData.monitorPointLockUnlockStatus = that.monitorPointLockUnlockStatus
+            that.submitForm(formData, that.saveApi)
           }
-        })
-        that.monitorPointLockUnlockStatus['taskEdit'] = saveParams['taskEdit']
-        if (JSON.stringify(that.monitorPointLockUnlockStatus) === '{}' && JSON.stringify(that.terminationSuspendStatus) === '{}') {
-          that.$message({ message: `请选择内容`, type: 'error' })
-        }
-        if (JSON.stringify(that.monitorPointLockUnlockStatus) !== '{}') {
-          formData.monitorPointLockUnlockStatus = that.monitorPointLockUnlockStatus
-          that.submitForm(formData, that.saveApi)
-        }
-        if (JSON.stringify(that.terminationSuspendStatus) !== '{}') {
-          if (that.terminationSuspendStatus['1025'] === '9020') {
-            that.$confirm('是否进行终止操作，此操作不可恢复', '提示', {
-              confirmButtonText: '是',
-              cancelButtonText: '否',
-              type: 'warning'
-            }).then(() => {
-              that.$api['planInfoManager.changeSwitchType']({ planInfoArray: that.planInfoArray, content: that.terminationSuspendStatus['1025'] }).then(res => {
+          if (JSON.stringify(that.terminationSuspendStatus) !== '{}') {
+            if (that.terminationSuspendStatus['1025'] === '9020') {
+              that
+                .$confirm('是否进行终止操作，此操作不可恢复', '提示', {
+                  confirmButtonText: '是',
+                  cancelButtonText: '否',
+                  type: 'warning'
+                })
+                .then(() => {
+                  that.$api['planInfoManager.changeSwitchType']({ planInfoArray: that.planInfoArray, content: that.terminationSuspendStatus['1025'] }).then((res) => {
+                    that.$message({
+                      message: '保存成功',
+                      type: 'success'
+                    })
+                    that.$emit('close-lock-unlock', {})
+                  })
+                })
+                .catch(() => {})
+            } else {
+              that.$api['planInfoManager.changeSwitchType']({ planInfoArray: that.planInfoArray, content: that.terminationSuspendStatus['1025'] }).then((res) => {
                 that.$message({
                   message: '保存成功',
                   type: 'success'
                 })
                 that.$emit('close-lock-unlock', {})
               })
-            }).catch(() => {
-            })
-          } else {
-            that.$api['planInfoManager.changeSwitchType']({ planInfoArray: that.planInfoArray, content: that.terminationSuspendStatus['1025'] }).then(res => {
-              that.$message({
-                message: '保存成功',
-                type: 'success'
-              })
-              that.$emit('close-lock-unlock', {})
-            })
+            }
           }
-        }
-      }).catch(() => {
-        console.error('异步失败的操作')
-      })
+        })
+        .catch(() => {
+          console.error('异步失败的操作')
+        })
     },
-    validate () {
+    validate() {
       return new Promise((resolve, reject) => {
-        this.$refs.form.validate(isValid => {
+        this.$refs.form.validate((isValid) => {
           if (isValid) {
             const queryParams = this.handleParams(Object.assign({}, this.formData))
             resolve(queryParams)
@@ -295,20 +272,22 @@ export default {
         })
       })
     },
-    submitForm (params, api) {
+    submitForm(params, api) {
       this.loadingVisible = true
-      this.saveForm(api, params).then((res) => {
-        this.successSave(res)
-      }).catch(function (e) {
-        console.error(e, '异步失败的操作')
-      })
+      this.saveForm(api, params)
+        .then((res) => {
+          this.successSave(res)
+        })
+        .catch(function (e) {
+          console.error(e, '异步失败的操作')
+        })
     },
-    handleParams (obj) {
+    handleParams(obj) {
       // 判断必须为obj
       if (!(Object.prototype.toString.call(obj) === '[object Object]')) {
         return {}
       }
-      let tempObj = {}
+      const tempObj = {}
       for (let [key, value] of Object.entries(obj)) {
         // if(value == null) value = '';
         if (Array.isArray(value) && value.length <= 0) continue
@@ -321,17 +300,19 @@ export default {
       }
       return tempObj
     },
-    saveForm (api, params, urlParam) {
-      let that = this
+    saveForm(api, params, urlParam) {
+      const that = this
       return new Promise((resolve, reject) => {
-        that.$api[api](params, urlParam).then(function (res) {
-          resolve(res)
-        }).catch(function (error) {
-          reject(error)
-        })
+        that.$api[api](params, urlParam)
+          .then(function (res) {
+            resolve(res)
+          })
+          .catch(function (error) {
+            reject(error)
+          })
       })
     },
-    successSave (res) {
+    successSave(res) {
       this.loadingVisible = false
       this.$message({
         message: '保存成功！',
