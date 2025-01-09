aad583c8ac7e65ad4fcab130ec22c337da695e30
mengyi <13572165143@163.com>
2025-01-09 15:33:53 +0800

    fix:1.2.1.10.148定位窗口关闭后甘特页面应当定位到关闭前的任务上，除了计划编制页面 其余页面都不生效

--- src/views/product/MonitoringGantt/index.vue
+++ src/views/product/MonitoringGantt/index.vue
@@ -238,9 +238,9 @@ export default {
       this.dialogVisible = false
       this.$store.getters.vueThis.searchForm = {}
       this.$store.getters.vueThisLocation.searchForm = {}
-      this.$refs.planGantt.relevancePlanVisible = false
-      this.$refs.planGantt.selectedId = this.$store.getters.vueThisLocation.selectTaskId
-      this.$refs.planGantt.initGantt(this.planInfoId, this.$refs.planGantt.viewType)
+      // this.$refs.planGantt.relevancePlanVisible = false
+      // this.$refs.planGantt.selectedId = this.$store.getters.vueThisLocation.selectTaskId
+      // this.$refs.planGantt.initGantt(this.planInfoId, this.$refs.planGantt.viewType)
     },
     onChangeTask (row) {
       let myGantt = GanttObject.getGanttObject(this.ganttName)
