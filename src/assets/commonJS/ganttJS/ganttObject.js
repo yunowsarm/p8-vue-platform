0428889df2b5d2d6d91ab59790d3956f09169942
liangmengyao <lmy_cn@163.com>
2025-01-16 15:53:15 +0800

    fix：修复任务变更时保存报错的bug

--- src/assets/commonJS/ganttJS/ganttObject.js
+++ src/assets/commonJS/ganttJS/ganttObject.js
@@ -2773,51 +2773,56 @@ GanttObject.updateTaskNew = function (ganttObject, taskId, vueThis) {
     updateforecastDate(task, ganttObject)
     ganttObject.updateTask(taskId)
     ganttObject.autoSchedule(taskId)
-    const parentId = task.parent
-    const parTasksIds = []
-    // 修改任务添加关联
-    if (vueThis.ganttName === 'changeGantt') {
-      if (ganttObject.isTaskExists(parentId)) {
-        const parTask = ganttObject.getTask(parentId)
-        // 更新父任务
-        parTasksIds.push(parTask.id)
-        ganttObject.eachParent(function (task) {
-          parTasksIds.push(task.id)
-        }, parTask.id)
-      }
-      if (parTasksIds && parTasksIds.length > 0) {
-        parTasksIds.forEach((t) => {
-          const parT = ganttObject.getTask(t)
-          updateforecastDate(parT, ganttObject)
-          ganttObject.updateTask(t)
-        })
-      }
-      // if (ganttObject.isTaskExists(parentId)) {
-      //   const parTask = ganttObject.getTask(parentId)
-      //   // 更新父任务
-      //   parTasksIds.push(parTask.id)
-      //   getTaskParent(ganttObject, parTask.id, parTasksIds)
-      // }
-      // // if (parTasksIds && parTasksIds.length > 0) {
-      // updateNewTaskMap(ganttObject, parTasksIds, vueThis, taskId)
-      // }
-    } else if (vueThis.ganttName === 'planGantt') {
-      if (ganttObject.isTaskExists(parentId)) {
-        const parTask = ganttObject.getTask(parentId)
-        // 更新父任务
-        parTasksIds.push(parTask.id)
-        ganttObject.eachParent(function (task) {
-          parTasksIds.push(task.id)
-        }, parTask.id)
-      }
-      if (parTasksIds && parTasksIds.length > 0) {
-        parTasksIds.forEach((t) => {
-          const parT = ganttObject.getTask(t)
-          updateforecastDate(parT, ganttObject)
-          ganttObject.updateTask(t)
-        })
+    if(task.parent){
+      const parentId = task.parent
+      const parentTask = ganttObject.getTask(parentId)
+      if(parentTask.autoScheduling !=='1') return
+      const parTasksIds = []
+      // 修改任务添加关联
+      if (vueThis.ganttName === 'changeGantt') {
+        if (ganttObject.isTaskExists(parentId)) {
+          const parTask = ganttObject.getTask(parentId)
+          // 更新父任务
+          parTasksIds.push(parTask.id)
+          ganttObject.eachParent(function (task) {
+            parTasksIds.push(task.id)
+          }, parTask.id)
+        }
+        if (parTasksIds && parTasksIds.length > 0) {
+          parTasksIds.forEach((t) => {
+            const parT = ganttObject.getTask(t)
+            updateforecastDate(parT, ganttObject)
+            ganttObject.updateTask(t)
+          })
+        }
+        if (ganttObject.isTaskExists(parentId)) {
+          const parTask = ganttObject.getTask(parentId)
+          // 更新父任务
+          parTasksIds.push(parTask.id)
+          getTaskParent(ganttObject, parTask.id, parTasksIds)
+        }
+        // if (parTasksIds && parTasksIds.length > 0) {
+        updateNewTaskMap(ganttObject, parTasksIds, vueThis, taskId)
+        // }
+      } else if (vueThis.ganttName === 'planGantt') {
+        if (ganttObject.isTaskExists(parentId)) {
+          const parTask = ganttObject.getTask(parentId)
+          // 更新父任务
+          parTasksIds.push(parTask.id)
+          ganttObject.eachParent(function (task) {
+            parTasksIds.push(task.id)
+          }, parTask.id)
+        }
+        if (parTasksIds && parTasksIds.length > 0) {
+          parTasksIds.forEach((t) => {
+            const parT = ganttObject.getTask(t)
+            updateforecastDate(parT, ganttObject)
+            ganttObject.updateTask(t)
+          })
+        }
       }
     }
+
   })
 }
 
