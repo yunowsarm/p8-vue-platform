0c2856c2372cbf832e938b3d82c63eef9ea4d31e
liangmengyao <lmy_cn@163.com>
2025-02-08 18:17:42 +0800

    fix: 计划变更-影响分析移除重复的变更状态列

--- src/assets/commonJS/ganttJS/analysisGanttObject.js
+++ src/assets/commonJS/ganttJS/analysisGanttObject.js
@@ -563,38 +563,6 @@ export function getAnalysisGantt (ganttName, vueThis) {
         }
       },
       {
-        name: 'changeStatusName',
-        label: '变更状态',
-        align: 'center',
-        width: 70,
-        resize: true,
-        template: function (task) {
-          let html = ''
-          if (ganttObject.getGlobalTaskIndex(task.id) !== 0) {
-            let infoType = task.infoType
-            let weatherChange = task.weatherChange
-            if (vueThis.ganttName && vueThis.ganttName === 'changeGantt' && weatherChange && weatherChange === '1') {
-              html = `<i class="gantt-tip p8 icon-change-item" style="color: #0d6bec;" title = "变更项"></i>`
-            }
-            if (infoType) {
-              switch (infoType) {
-                case 'create':
-                  html = `<i class="gantt-tip p8 icon-make-increase" style="color: #0d6bec;" title="调增"></i>`
-                  break
-                case 'update':
-                  html = `<i class="gantt-tip p8 icon-content-adjustment" style="color: #0d6bec;" title = "内容调整"></i>`
-                  break
-                case 'delete':
-                  html = `<i class="gantt-tip p8 icon-make-reductions" style="color: #0d6bec;" title = "调减"></i>`
-                  break
-              }
-            }
-            html += task.changeStatusName || ''
-          }
-          return html
-        }
-      },
-      {
         name: 'monitorPoints',
         label: '标识',
         align: 'left',
