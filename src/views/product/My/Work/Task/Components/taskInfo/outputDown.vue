b6f58a9d9c467ee3036c65cfbdea03b47bd26d63
tangning <1039928300@qq.com>
2025-03-07 16:10:28 +0800

    feat: 列表前后置下钻页面输出列，展示数量，点击可以下钻页面-列表为序号、输出物名称，附件名称、创建时间。点击名称可下载附件

---
+++ src/views/product/My/Work/Task/Components/taskInfo/outputDown.vue
@@ -0,0 +1,49 @@
+<template>
+  <table-render code="outputInfo"
+                :report-param="reportParam1">
+                <template #FILE_NAME="{ scope }">
+                    <span style="text-decoration:underline;cursor: pointer;" @click="fileDownLoad(scope.row)">{{scope.row.FILE_NAME}}</span>
+                </template>
+  </table-render>
+</template>
+<script>
+import TableRender from '@/views/Framework/ComponentsMananger/Grid/Components/tableRender'
+export default {
+  name: 'NormalLayoutAndTree',
+  components: {
+    TableRender
+  },
+  props: ['reportParam'],
+  data () {
+    return {
+      reportParam1: {
+        ID: ''
+      }
+    }
+  },
+  created () {
+    this.reportParam1.ID = this.reportParam.TASKID ? this.reportParam.TASKID : ''
+  },
+  methods: {
+    fileDownLoad (row) {
+        // 输出要求-文件下载
+        if (row.IO_ID) {
+            this.$api['documentManagement.download']({ attachmentId: row.IO_ID }, { responseType: 'blob' })
+            .then((backJson) => {
+                const link = document.createElement('a')
+                link.href = window.URL.createObjectURL(new Blob([backJson.data]))
+                link.download = row.FILE_NAME
+                document.body.appendChild(link)
+
+                link.click()
+                window.URL.revokeObjectURL(link.href)
+                document.body.removeChild(link)
+            })
+            .finally(() => {
+                // this.search.exportLoading = false
+            })
+        }
+    }
+  }
+}
+</script>
