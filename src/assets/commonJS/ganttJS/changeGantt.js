e6e751dbd0c5a7bc4ab4120b7dde34be258bec76
mengyi <13572165143@163.com>
2025-04-24 17:30:53 +0800

    fix:变更列表中点击修改进入变更页面，点击右下角保存，会提示需先修改才可保存，实际已有修改数据

--- src/assets/commonJS/ganttJS/changeGantt.js
+++ src/assets/commonJS/ganttJS/changeGantt.js
@@ -204,7 +204,8 @@ export function taskDescribesEditCheck(newObj, oldObj, vueThis, taskId, ganttObj
           task.autoScheduling = newObj.autoScheduling
         }
       }
-    } else if (oldObj[key] !== newObj[key]) {
+    }
+    if (oldObj[key] !== newObj[key]) {
       checkChange = true
       if (key === 'describes' && oldObj[key] !== newObj[key]) {
         describesEdit = true
