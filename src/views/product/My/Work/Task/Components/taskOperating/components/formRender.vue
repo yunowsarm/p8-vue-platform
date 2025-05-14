09f79c02deec5afc88362fbb24b04938cec8940e
tangning <1039928300@qq.com>
2025-05-14 09:12:26 +0800

    fix: 任务100%提交审批后，业务表单没有同步提交（变为只读），还需点击一次业务表单下方的提交才可变为只读，需优化

--- src/views/product/My/Work/Task/Components/taskOperating/components/formRender.vue
+++ src/views/product/My/Work/Task/Components/taskOperating/components/formRender.vue
@@ -1,11 +1,14 @@
 <template>
-  <form-render ref="form"
-               style="padding-top: 20px;"
-               :dataViewId="formViewId"
-               :record="{ desformCode: formCode }"
-               :key="timeKey"
-               :pageType="pageType"
-               @save-success="(res) => formCloseRefresh(res)"></form-render>
+  <div style="position: relative;">
+    <form-render ref="form"
+                style="padding-top: 20px;"
+                :dataViewId="formViewId"
+                :record="{ desformCode: formCode }"
+                :key="timeKey"
+                :pageType="pageType"
+                @save-success="(res) => formCloseRefresh(res)"></form-render>
+    <div v-if="pageType == 'view' && !formViewId" class="single-mask" ></div>
+  </div>
 </template>
 
 <script>
@@ -36,7 +39,7 @@ export default {
   components: {
     FormRender
   },
-  async created () {
+  created () {
     this.getInfo()
   },
 
@@ -71,4 +74,13 @@ export default {
 
 </script>
 <style lang='scss' scoped>
+.single-mask {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.1);
+  z-index: 999;
+}
 </style>
