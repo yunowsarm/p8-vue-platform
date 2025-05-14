567d6cb20ebe2be5c0b009ab4e897f4911f3aa61
mengyi <13572165143@163.com>
2025-05-14 10:22:11 +0800

    fix:任务粘贴之后从新获取任务总数

--- src/assets/commonJS/ganttJS/commandButtonData.js
+++ src/assets/commonJS/ganttJS/commandButtonData.js
@@ -3653,6 +3653,7 @@ function pasteTask (ganttObject, tasks, vueThis, type, dpObj) {
                 el.owner_id = ''
               })
               createTaskByDatas(ganttObject, res.tasks, parentTask.id, 'paste', null, '任务粘贴成功！', dpObj, selIndexNo)
+              vueThis.taskCount = ganttObject.getTaskCount()
             }
           } else {
             vueThis.$message({
