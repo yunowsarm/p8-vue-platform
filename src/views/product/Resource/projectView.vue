d010e1b16b1624cfcf14be1ab0c1f114ecb7b377
mengyi <13572165143@163.com>
2025-05-19 15:54:09 +0800

    fix：主题样式下的漏网之鱼优化

--- src/views/product/Resource/projectView.vue
+++ src/views/product/Resource/projectView.vue
@@ -486,7 +486,7 @@ export default {
       this.memberFormComp = formComp
     },
     search (params) {
-       this.reset()
+      this.reset()
       this.searchParam = params
       let realName = params.realName ? params.realName : null
       let deptName = params.deptName ? params.deptName : null
@@ -1180,10 +1180,10 @@ export default {
 
       &.active {
         font-weight: bolder;
-        color: #0e77d1 !important;
+        color: $theme-color !important;
         ::v-deep span {
           font-weight: bolder;
-          color: #0e77d1 !important;
+          color: $theme-color !important;
         }
       }
 
@@ -1193,7 +1193,7 @@ export default {
         left: 0;
         top: 0;
         height: 100%;
-        border-left: 3px solid #46a6ff;
+        border-left: 3px solid $theme-color;
       }
 
       i {
