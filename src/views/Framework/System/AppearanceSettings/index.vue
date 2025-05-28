5b21fc67b7cc354df5ab2d1a1dc6989c8d6508ba
mengyi <13572165143@163.com>
2025-05-28 10:38:02 +0800

    fix：个性化设置优化

--- src/views/Framework/System/AppearanceSettings/index.vue
+++ src/views/Framework/System/AppearanceSettings/index.vue
@@ -217,25 +217,25 @@
                                 content="查看">
                       <el-button style="margin-right: 2px;"
                                  icon="p8 icon-chakan"
-                                 type="primary"></el-button>
+                                 type="text"></el-button>
                     </el-tooltip>
                     <el-tooltip placement="top"
                                 content="删除">
                       <el-button icon="p8 icon-shanchu"
-                                 type="primary"></el-button>
+                                 type="text"></el-button>
                     </el-tooltip>
                   </el-button-group>
                   <div v-else>
                     <el-tooltip placement="top"
                                 content="查看">
                       <el-button icon="p8 icon-chakan"
-                                 type="primary"></el-button>
+                                 type="text"></el-button>
                     </el-tooltip>
                     <el-divider direction="vertical"></el-divider>
                     <el-tooltip placement="top"
                                 content="删除">
                       <el-button icon="p8 icon-shanchu"
-                                 type="primary"></el-button>
+                                 type="text"></el-button>
                     </el-tooltip>
                   </div>
                 </div>
@@ -243,16 +243,16 @@
                   <el-button-group v-if="formData.toolbarCompactLayout">
                     <el-button style="margin-right: 2px;"
                                icon="p8 icon-chakan"
-                               type="primary">查看</el-button>
+                               type="text">查看</el-button>
                     <el-button icon="p8 icon-shanchu"
-                               type="primary">删除</el-button>
+                               type="text">删除</el-button>
                   </el-button-group>
                   <div v-else>
                     <el-button icon="p8 icon-chakan"
-                               type="primary">查看</el-button>
+                               type="text">查看</el-button>
                     <el-divider direction="vertical"></el-divider>
                     <el-button icon="p8 icon-shanchu"
-                               type="primary">删除</el-button>
+                               type="text">删除</el-button>
                   </div>
                 </div>
               </template>
