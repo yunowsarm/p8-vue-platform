88ffccabd613f440a27dcc5ee02c84995b44c8be
tangning <1039928300@qq.com>
2025-06-25 18:15:22 +0800

    fix: 质量管理增加消息查看页面

---
+++ src/views/product/problemManagement/messageView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="height: 100%;">
+    <P8FormDataEdit :record="{ desformCode: 'problemManagement' }"
+                    :dataViewId="taskId"
+                    page-type="view" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'messageView',
+  props: ['taskId'],
+}
+
+</script>
