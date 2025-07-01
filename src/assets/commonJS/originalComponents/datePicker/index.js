f62bef1485e6813ae8890f6f7ecf84df44771944
liangmengyao <lmy_cn@163.com>
2025-07-01 18:36:11 +0800

    feat（gantt）：gantt列表-表头查询，计划开始时间和计划完成时间修改为区间搜索

--- src/assets/commonJS/originalComponents/datePicker/index.js
+++ src/assets/commonJS/originalComponents/datePicker/index.js
@@ -1,48 +1,89 @@
 import DOMJS from '@/assets/commonJS/originalComponents/utils/dom'
 import DatePanel from './panel'
 import moment from 'moment'
+
 export default class DatePicker {
-  constructor (parentEle, options) {
+  constructor(parentEle, options) {
     this._init(parentEle, options)
   }
 
-  _init (parentEle, {
-    value = '',
+  _init(parentEle, {
+    value = [],
     customClassName = '',
     placeholder = '选择日期',
-    onChange
+    onChange,
+    range = false
   }) {
     this.parentEle = document.querySelector(parentEle) || document.body
     this.value = value
     this.customClassName = customClassName
     this.placeholder = placeholder
     this.onChange = onChange
+    this.range = range
     this.ele = {}
     this.dpPanelShow = false
     this._createElement()
   }
 
-  _createElement () {
+  _createElement() {
     const _this = this
     let fragment = document.createDocumentFragment()
     // 1. 创建datePicker根节点
     let dpRoot = DOMJS.createElement('div', `cs-date-picker ${this.customClassName}`)
+    let inputValue = ''
+    if (this.range && Array.isArray(this.value)) {
+      inputValue = this.value[0] && this.value[1] ? `${moment(this.value[0]).format('YYYY-MM-DD')} ~ ${moment(this.value[1]).format('YYYY-MM-DD')}` : ''
+    } else {
+      inputValue = this.value ? moment(this.value).format('YYYY-MM-DD') : ''
+    }
     // 2. 创建Input整体节点
-    let { inputWrap, input, spanIconWrap, icon } = DOMJS.createInput(this.value ? moment(this.value).format('YYYY-MM-DD') : '', this.placeholder)
+    let { inputWrap, input, spanIconWrap, icon } = DOMJS.createInput(inputValue, this.placeholder)
     this.ele.inputWrap = inputWrap
     this.ele.input = input
     this.ele.spanIconWrap = spanIconWrap
     this.ele.inputIcon = icon
+    const rawValue = Array.isArray(_this.value) ? _this.value[0] : _this.value
+    let date
+    if (rawValue &&
+      !(Array.isArray(_this.value) && _this.value.length === 0) &&
+      !isNaN(new Date(rawValue).getTime())
+    ) {
+      date = new Date(rawValue)
+    } else{
+      date = new Date()
+    }
     // 3. 创建datePicker弹窗面板
     this.ele.dpPanel = new DatePanel({
       value: _this.value,
-      date: (_this.value && new Date(_this.value)) || new Date(),
-      dateCellClick (v) {
-        let { date, year, month } = v
-        _this.value = date
-        _this.ele.input.value = moment(date).format('YYYY-MM-DD')
-        _this._selectDropup()
-        _this.onChange && _this.onChange({ date, year, month })
+      date: date,
+      range: this.range,
+      dateCellClick(v) {
+        if (this.range) {
+          if (!_this._rangeSelecting) {
+            _this._rangeSelecting = true
+            _this._rangeStart = v.date
+            _this._rangeEnd = null
+            _this.ele.input.value = moment(v.date).format('YYYY-MM-DD') + ' ~ '
+          } else {
+            // _this.rangeEnd = v.date
+            let start = v.range ? v.range[0] : _this._rangeStart
+            let end =  v.range ? v.range[1] : v.date
+            if (start > end) [start, end] = [end, start]
+            _this.value = [start, end]
+            _this.ele.input.value = `${moment(start).format('YYYY-MM-DD')} ~ ${moment(end).format('YYYY-MM-DD')}`
+            _this._selectDropup()
+            _this.onChange && _this.onChange({ value: [moment(start).format('YYYY-MM-DD'), moment(end).format('YYYY-MM-DD')] })
+            _this._rangeSelecting = false
+            _this._rangeStart = null
+            _this._rangeEnd = null
+          }
+        } else {
+          let { date, year, month } = v
+          _this.value = date
+          _this.ele.input.value = moment(date).format('YYYY-MM-DD')
+          _this._selectDropup()
+          _this.onChange && _this.onChange({ date, year, month })
+        }
       }
     })
     DOMJS.appendChild(dpRoot, inputWrap)
@@ -54,14 +95,14 @@ export default class DatePicker {
   }
 
   // 收起下拉选项
-  _selectDropup () {
+  _selectDropup() {
     this.dpPanelShow = false
     DOMJS.setStyle(this.ele.dpPanel.root, 'transform', 'scale(1,0)')
     DOMJS.setStyle(this.ele.dpPanel.root, 'opacity', '0')
   }
 
   // 展示下拉选项
-  _selectDropdown () {
+  _selectDropdown() {
     this.dpPanelShow = true
     DOMJS.setStyle(this.ele.dpPanel.root, 'transform', 'scale(1,1)')
     DOMJS.setStyle(this.ele.dpPanel.root, 'opacity', '1')
@@ -69,15 +110,15 @@ export default class DatePicker {
   }
 
   // 更新input icon 类名(箭头 or 清空)
-  _updataSelectIconClass (type) {
+  _updataSelectIconClass(type) {
     if (type === 'enter') {
-        this.ele.inputIcon.className = 'cs-input-icon cs-input-icon__clear el-icon-circle-close'
+      this.ele.inputIcon.className = 'cs-input-icon cs-input-icon__clear el-icon-circle-close'
     } else {
-        this.ele.inputIcon.className = 'cs-input-icon el-icon-arrow-down'
+      this.ele.inputIcon.className = 'cs-input-icon el-icon-arrow-down'
     }
   }
 
-  _clear () {
+  _clear() {
     this.vlaue = ''
     this.ele.input.value = ''
     this.ele.dpPanel.date = new Date()
@@ -87,7 +128,7 @@ export default class DatePicker {
     this.onChange && this.onChange({})
   }
 
-  _bind () {
+  _bind() {
     const _this = this
     this.ele.inputWrap.addEventListener('click', (e) => {
       const target = e.target
@@ -98,10 +139,10 @@ export default class DatePicker {
       }
     })
 
-    this.ele.inputWrap.addEventListener('mouseenter', function (e) {
+    this.ele.inputWrap.addEventListener('mouseenter', function(e) {
       _this.value && _this._updataSelectIconClass('enter')
     })
-    this.ele.inputWrap.addEventListener('mouseleave', function (e) {
+    this.ele.inputWrap.addEventListener('mouseleave', function(e) {
       _this.value && _this._updataSelectIconClass('leave')
     })
   }
