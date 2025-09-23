0b23a2949b7b96fa644e8bb067dd907603ae3867
liangmengyao <lmy_cn@163.com>
2025-09-23 17:00:09 +0800

    feat(1.4.49.12): 主页看板 - 工作项统计图表 - 新增待处理问题，支持点击查看和处理反馈

---
+++ src/views/product/HomeBoardOne/components/MineQuestion.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'MineQuestion'
+}
+</script>
+
+<template>
+  <P8TableRender
+    ref="tableRender"
+    code="problemManagement"
+    specialRoteName="My_ProblemList"
+    :reportParam="{
+      showView: {
+        mode: '=',
+        relation: 'and',
+        value: 'showView002'
+      }
+    }"
+  ></P8TableRender>
+</template>
+
+<style scoped lang="scss"></style>
