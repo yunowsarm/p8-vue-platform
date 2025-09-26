902b7371deb00153b7c466ce48f036651d1c067c
liangmengyao <lmy_cn@163.com>
2025-09-26 17:19:08 +0800

    fix(1.21.45): 预算变更记录根据项目id过滤

---
+++ src/views/product/BudgetManagement/BudgetChange/index.vue
@@ -0,0 +1,38 @@
+<script>
+import formView from '@/views/product/problemManagement/formView.vue'
+import { P8Drawer as CommonDrawer } from 'p8-components-ui'
+export default {
+  name: 'BudgetChange',
+  props:{
+    thirdMenuParam: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    },
+  },
+  data(){
+    return {
+      reportParam:{
+        WHOLEID: {
+          mode: '=',
+          relation: 'and',
+          value: this.thirdMenuParam.WHOLEID
+        }
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <P8TableRender
+      ref="tableRender"
+      code="wholeBudgetChangeTable"
+      :reportParam="reportParam"
+    ></P8TableRender>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
