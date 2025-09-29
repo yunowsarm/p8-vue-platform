5b68946cf3404b53774199c083edb3cdc78198e0
liangmengyao <lmy_cn@163.com>
2025-09-29 19:13:29 +0800

    feat(1.5.98): 集成管理模块功能代码提交

--- src/views/Framework/IntegratedManagement/ServerSide/index.vue
+++ src/views/Framework/IntegratedManagement/ServerSide/index.vue
@@ -1,96 +1,19 @@
-<script>
-import { P8Tabs as CommonTabs } from 'p8-components-ui'
-import InterfaceManagement from '../components/InterfaceManagement/index.vue'
-import ExecutionLog from '../components/ExecutionLog/index.vue'
-import ExceptionMonitoring from '../components/ExceptionMonitoring/index.vue'
-
-
-export default {
-  name: 'ServerSide',
-  components: {
-    CommonTabs,
-    InterfaceManagement,
-    ExecutionLog,
-    ExceptionMonitoring
-  },
-  provide(){
-    return {
-      type: "server",
-      interFace:{
-        list:'integratedManagement.serverList',
-        data:'integratedManagement.serverData',
-        save:'integratedManagement.serverSave',
-        delete:'integratedManagement.serverDelete',
-        debug:'integratedManagement.serverDebug'
-      }
-    }
-  },
-  data () {
-    return {
-      activeTabs:'interfaceManagement',
-      tabsData: [
-        {
-          label: '接口管理',
-          name: 'interfaceManagement'
-        },
-        {
-          label: '执行日志',
-          name: 'executionLog'
-        },
-        {
-          label: '异常监控',
-          name: 'exceptionMonitoring'
-        }
-      ]
-    }
-  }
-}
-</script>
-
 <template>
-  <div id="ServerSide">
-    <common-tabs :tabs-data="tabsData"
-                 class="el_tabs"
-                 :active-tabs="activeTabs">
-      <template #interfaceManagement>
-        <InterfaceManagement></InterfaceManagement>
-      </template>
-      <template #executionLog>
-        <ExecutionLog></ExecutionLog>
-      </template>
-      <template #exceptionMonitoring>
-        <ExceptionMonitoring></ExceptionMonitoring>
-      </template>
-    </common-tabs>
-  </div>
+  <P8ServerSide />
 </template>
 
-<style scoped lang="scss">
-#ServerSide{
-  box-sizing: border-box;
-  height: calc(100% - 32px);
-  width: calc(100% - 32px);
-  margin: 16px;
-  background: #FFFFFF;
-  .p8-tabs{
-    height: 100%;
-  }
+<script>
+export default {
+  name: 'ServerSide',
+  data () {
+    return {
+    }
+  },
+  methods: {
+
+  },
+  created () {
+    console.log('P8ServerSide')
+  },
 }
-::v-deep .list-layout {
-  margin: 0;
-  height: 100%;
-}
-::v-deep .el-tabs__header{
-  margin: 0;
-}
-::v-deep .el-tabs__item{
-  padding: 0 20px !important;
-}
-::v-deep .el-tabs--top .el-tabs__content{
-  height: calc(100% - 40px);
-  padding: 0;
-}
-::v-deep .search-wrapper{
-  right: 0;
-}
-</style>
+</script>
