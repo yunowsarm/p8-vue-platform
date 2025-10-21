fa7d03b6b3ccf896194d0ae80098418f6493b207
mengyi <13572165143@163.com>
2025-10-21 13:39:49 +0800

    fix:提交配置文件版本

--- public/static/config.js
+++ public/static/config.js
@@ -10,10 +10,10 @@ const CONFIG = {
   // devBaseUrl: 'http://192.168.0.101:9099', // 第一次执行参考以上备注
   // devBaseUrl: 'http://192.168.0.146:9091', // 第一次执行参考以上备注
   // devBaseUrl: defaultServiceAddr, // 第一次执行参考以上备注
-  devBaseUrl: 'http://192.168.0.225:9994', // 第一次执行参考以上备注
+  devBaseUrl: 'http://192.168.0.225:9995', // 第一次执行参考以上备注
   // devBaseUrl: 'http://192.168.0.142:9092',
   // jmreportUrl:'http://192.168.0.142:9092/jmreport',
-  aiUrl:'http://192.168.0.18',
+  aiUrl: 'http://192.168.0.18',
   jmreportUrl: defaultServiceAddr + '/jmreport',
   prodBaseUrl: defaultServiceAddr // 自动获取浏览器地址栏地址作为后端访问地址，适应于前端程序部署在后端的Web服务器容器内的模式，如果分离部署模式，请修改为后台访问地址。
 }
@@ -27,4 +27,4 @@ const loginCa = false
 // eslint-disable-next-line no-unused-vars
 const CHECK_PRODUCTION_ERROR_TIP = false
 // 聊天机器人
-const aiAssistant = false
+const aiAssistant = true
