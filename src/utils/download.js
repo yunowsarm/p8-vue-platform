82498400a85a5951a841c18890352e3a6c0feda5
liangmengyao <lmy_cn@163.com>
2025-11-25 16:56:25 +0800

    feat(1.22.4): 适配移动端表格导出功能

--- src/utils/download.js
+++ src/utils/download.js
@@ -1,47 +1,49 @@
 import Vue from 'vue'
-export function download (id, name, type) {
+import { getToken } from '@/service/expands/auth'
+
+export function download(id, name, type) {
   if (window.plus) {
     const { API_DEFAULT_CONFIG } = Vue.prototype.$sysConfig
-    const { downloadUrl,downloadTemplateUrl } = API_DEFAULT_CONFIG;
+    const { exportUrl, downloadUrl, downloadTemplateUrl } = API_DEFAULT_CONFIG
     let url
     if (type === 'template') {
       url = `${downloadTemplateUrl}?attachmentId=${id}`
+    } else if ('export') {
+      url = `${exportUrl}?isMobile=true&key=${id}&authorization=${getToken()}`
     } else {
       url = `${downloadUrl}?attachmentId=${id}`
     }
     const savePath = `_downloads/${name}`
-    function getFriendlyPath(fullPath){
-      if(!fullPath) return ''
+
+    function getFriendlyPath(fullPath) {
+      if (!fullPath) return ''
       const index = fullPath.indexOf('Android/data/')
-      if(index >= 0){
+      if (index >= 0) {
         return fullPath.substring(index)
       }
       return fullPath.substring(fullPath.lastIndexOf('/') + 1)
     }
+
     plus.nativeUI.showWaiting('正在下载...')
-    const dtask = plus.downloader.createDownload(
-      url,
-      { filename: savePath },
-      (download, status) => {
-        plus.nativeUI.closeWaiting()
-        if (status === 200) {
-          plus.io.resolveLocalFileSystemURL(download.filename,entry => {
-            const friendlyPath = getFriendlyPath(entry.fullPath)
-            plus.nativeUI.alert(
-              `文件已下载成功：\n保存位置：${friendlyPath}。`,
-              () => {
-                plus.runtime.openFile(entry.fullPath, {}, (err) => {
-                  plus.nativeUI.toast('无法打开,请手动到下载目录查看')
-                })
-              },
-              '下载完成'
-            )
-          })
-        } else {
-          plus.nativeUI.toast('下载失败，请检查网络或地址')
-        }
+    const dtask = plus.downloader.createDownload(url, { filename: savePath }, (download, status) => {
+      plus.nativeUI.closeWaiting()
+      if (status === 200) {
+        plus.io.resolveLocalFileSystemURL(download.filename, (entry) => {
+          const friendlyPath = getFriendlyPath(entry.fullPath)
+          plus.nativeUI.alert(
+            `文件已下载成功：\n保存位置：${friendlyPath}。`,
+            () => {
+              plus.runtime.openFile(entry.fullPath, {}, (err) => {
+                plus.nativeUI.toast('无法打开,请手动到下载目录查看')
+              })
+            },
+            '下载完成'
+          )
+        })
+      } else {
+        plus.nativeUI.toast('下载失败，请检查网络或地址')
       }
-    )
+    })
     dtask.start()
   } else {
     const api = Vue.prototype.$api
@@ -55,6 +57,6 @@ export function download (id, name, type) {
         window.URL.revokeObjectURL(link.href)
         document.body.removeChild(link)
       })
-      .finally(() => { })
+      .finally(() => {})
   }
 }
